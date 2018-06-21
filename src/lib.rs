742cac96c3fe8da7a24ff0150e0f8de5839f2e04
Nikita Baksalyar <nikita.baksalyar@gmail.com>
2018-06-21 14:17:54 +0300

    test/all: add unit tests for encryption

--- src/lib.rs
+++ src/lib.rs
@@ -198,3 +198,128 @@ quick_error! {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn anonymous_bytes_cipher() {
+        let data = b"Humpty Dumpty sat on a wall";
+        let sk = SecretId::new();
+        let sk2 = SecretId::new();
+        let pk = sk.public_id();
+
+        let ciphertext = pk.encrypt_anonymous_bytes(data);
+        assert!(&ciphertext != data);
+
+        let error_res: Result<_, _> = sk.decrypt_anonymous_bytes(data);
+        match error_res {
+            Err(_e) => (),
+            Ok(_) => {
+                panic!("Unexpected result: we're using wrong data, it should have returned error")
+            }
+        }
+
+        let error_res: Result<_, _> = sk2.decrypt_anonymous_bytes(&ciphertext);
+        match error_res {
+            Err(_e) => (),
+            Ok(_) => {
+                panic!("Unexpected result: we're using a wrong key, it should have returned error")
+            }
+        }
+
+        let plaintext: Vec<u8> = sk
+            .decrypt_anonymous_bytes(&ciphertext)
+            .expect("couldn't decrypt ciphertext");
+        assert!(&plaintext == data);
+    }
+
+    #[test]
+    fn anonymous_cipher() {
+        let data: Vec<u64> = vec![4, 5, 6];
+
+        let sk = SecretId::new();
+        let pk = sk.public_id();
+
+        let ciphertext = pk
+            .encrypt_anonymous(&data)
+            .expect("couldn't encrypt base data");
+        assert!(!ciphertext.is_empty());
+
+        let plaintext: Vec<u64> = sk
+            .decrypt_anonymous(&ciphertext)
+            .expect("couldn't decrypt ciphertext");
+        assert!(plaintext == data);
+    }
+
+    #[test]
+    fn authenticated_cipher() {
+        let data = b"Humpty Dumpty had a great fall.";
+
+        let sk1 = SecretId::new();
+        let pk1 = sk1.public_id();
+
+        let sk2 = SecretId::new();
+        let pk2 = sk2.public_id();
+
+        let shared_key1 = sk1.shared_key(&pk2);
+        let shared_key2 = sk2.shared_key(&pk1);
+
+        let ciphertext = shared_key1
+            .encrypt_bytes(data)
+            .expect("couldn't encrypt data");
+        assert!(&ciphertext != data);
+
+        let plaintext = shared_key2
+            .decrypt_bytes(&ciphertext)
+            .expect("couldn't decrypt data");
+        assert!(&plaintext == data);
+
+        // Trying with wrong data
+        let error_res: Result<_, _> = shared_key2.decrypt_bytes(&plaintext);
+        match error_res {
+            Err(_e) => (),
+            Ok(_) => {
+                panic!("Unexpected result: we're using wrong data, it should have returned error")
+            }
+        }
+
+        // Trying with a wrong key
+        let sk3 = SecretId::new();
+        let shared_key3 = sk3.shared_key(&pk2);
+
+        let error_res: Result<_, _> = shared_key3.decrypt_bytes(&ciphertext);
+        match error_res {
+            Err(_e) => (),
+            Ok(_) => {
+                panic!("Unexpected result: we're using a wrong key, it should have returned error")
+            }
+        }
+    }
+
+    #[test]
+    fn signing() {
+        let data1 = b"All the king's horses and all the king's men";
+        let data2 = b"Couldn't put Humpty together again";
+
+        let sk1 = SecretId::new();
+        let pk1 = sk1.public_id();
+
+        let sk2 = SecretId::new();
+        let pk2 = sk2.public_id();
+
+        let sig1 = sk1.sign_detached(data1);
+        let sig2 = sk2.sign_detached(data2);
+
+        assert_eq!(pk1.verify_detached(&sig1, data1), true);
+        assert_eq!(pk1.verify_detached(&sig1, data2), false);
+        assert_eq!(pk1.verify_detached(&sig2, data1), false);
+        assert_eq!(pk1.verify_detached(&sig2, data2), false);
+
+        assert_eq!(pk2.verify_detached(&sig1, data1), false);
+        assert_eq!(pk2.verify_detached(&sig1, data2), false);
+        assert_eq!(pk2.verify_detached(&sig2, data1), false);
+        assert_eq!(pk2.verify_detached(&sig2, data2), true);
+    }
+}
